1f19248003bd10c5309682ec51033e6abfa19159
carver <niwelocarver88@gmail.com>
2025-03-18 21:50:28 +0100

    nah

--- pitching_simulator.py
+++ pitching_simulator.py
@@ -162,24 +162,27 @@ class JuegoBeisbol:
 
     def __init__(self, equipo_local='Vencejos', equipo_visitante="Dodgers"):
         self.jugadores = [
-            Jugador("Shohei Ohtani", 0.307, 0.392, 70, 25, 5, 35, 550, 'L'),
             Jugador("Mookie Betts", 0.289, 0.370, 100, 28, 3, 24, 650, 'R'),
-            Jugador("Freddie Freeman", 0.341, 0.410, 85, 50, 0, 29, 600, 'L'),
-            Jugador("Teoscar Hernández", 0.267, 0.316, 90, 25, 1, 33, 560, 'R'),
-            Jugador("Max Muncy", 0.212, 0.333, 60, 20, 0, 36, 580, 'L'),
-            Jugador("Tommy Edman", 0.265, 0.324, 90, 20, 5, 13, 600, 'R'),
-            Jugador("Michael Conforto", 0.238, 0.330, 70, 15, 0, 15, 400, 'L'),
-            Jugador("Will Smith", 0.248, 0.330, 80, 20, 0, 20, 500, 'R'),
-            Jugador("Chris Taylor", 0.240, 0.300, 85, 15, 5, 10, 450, 'L'),
-            Jugador("Esteury Ruiz", 0.250, 0.310, 100, 15, 5, 5, 500, 'R'),
-            Jugador("Zach Gelof", 0.240, 0.320, 85, 20, 2, 15, 550, 'R'),
-            Jugador("Lawrence Butler", 0.260, 0.330, 90, 25, 3, 20, 580, 'L'),
-            Jugador("Brent Rooker", 0.230, 0.310, 70, 15, 0, 25, 500, 'R'),
-            Jugador("Seth Brown", 0.220, 0.300, 60, 20, 2, 20, 480, 'L'),
-            Jugador("Gio Urshela", 0.270, 0.320, 95, 25, 1, 10, 550, 'R'),
-            Jugador("Luis Urías", 0.245, 0.330, 80, 20, 1, 15, 520, 'R'),
-            Jugador("Jhonny Pereda", 0.230, 0.310, 60, 15, 0, 10, 400, 'R'),
-            Jugador("Jacob Wilson", 0.250, 0.320, 85, 20, 2, 8, 500, 'R')
+            Jugador("Mookie Betts", 0.289, 0.370, 100, 28, 3, 24, 650, 'R'),
+            Jugador("Mookie Betts", 0.289, 0.370, 100, 28, 3, 24, 650, 'R'),
+            Jugador("Mookie Betts", 0.289, 0.370, 100, 28, 3, 24, 650, 'R'),
+            Jugador("Mookie Betts", 0.289, 0.370, 100, 28, 3, 24, 650, 'R'),
+            Jugador("Mookie Betts", 0.289, 0.370, 100, 28, 3, 24, 650, 'R'),
+            Jugador("Mookie Betts", 0.289, 0.370, 100, 28, 3, 24, 650, 'R'),
+            Jugador("Mookie Betts", 0.289, 0.370, 100, 28, 3, 24, 650, 'R'),
+            Jugador("Mookie Betts", 0.289, 0.370, 100, 28, 3, 24, 650, 'R'),
+            Jugador("Mookie Betts", 0.289, 0.370, 100, 28, 3, 24, 650, 'R'),
+            Jugador("Mookie Betts", 0.289, 0.370, 100, 28, 3, 24, 650, 'R'),
+            Jugador("Mookie Betts", 0.289, 0.370, 100, 28, 3, 24, 650, 'R'),
+            Jugador("Mookie Betts", 0.289, 0.370, 100, 28, 3, 24, 650, 'R'),
+            Jugador("Mookie Betts", 0.289, 0.370, 100, 28, 3, 24, 650, 'R'),
+            Jugador("Mookie Betts", 0.289, 0.370, 100, 28, 3, 24, 650, 'R'),
+            Jugador("Mookie Betts", 0.289, 0.370, 100, 28, 3, 24, 650, 'R'),
+            Jugador("Mookie Betts", 0.289, 0.370, 100, 28, 3, 24, 650, 'R'),
+            Jugador("Mookie Betts", 0.289, 0.370, 100, 28, 3, 24, 650, 'R'),
+            Jugador("Mookie Betts", 0.289, 0.370, 100, 28, 3, 24, 650, 'R'),
+            Jugador("Mookie Betts", 0.289, 0.370, 100, 28, 3, 24, 650, 'R'),
+            Jugador("Mookie Betts", 0.289, 0.370, 100, 28, 3, 24, 650, 'R')
         ]
 
         if equipo_visitante == "Dodgers":
@@ -364,7 +367,7 @@ class JuegoBeisbol:
         mod_calidad = self.MOD_CALIDAD[calidad]
         #prob_hit_ini = calcular_porcentage(self.bateador_actual.avg)
         prob_hit = self.bateador_actual.avg * mod_tipo / mod_calidad
-        prob_hit *= 1.55
+        prob_hit *= 1.515
         
         if random.random() < prob_hit:
             return self._generar_hit()
